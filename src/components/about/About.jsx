0572a94f8199d7270417b7cedea44a49dd349907
Louis-Philippe Simard <64223102+Louispoire@users.noreply.github.com>
2025-03-27 12:33:38 -0400

    Added animation
    Improved navbar

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,30 +1,52 @@
 import './About.css';
 import { motion } from 'framer-motion';
 import profilePic from '../../assets/profile.jpg'; // Make sure the image exists in your assets folder
-import {FaEnvelope} from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
 
 function About() {
   return (
     <section className="about">
       <div className="about-container">
         {/* Text Section */}
-        <div className="about-text">
-          <h2>About Me</h2>
+        <motion.div
+          className='about-text'
+          initial={{ opacity: 0, x: -40 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+          viewport={{ once: true, amount: 0.2 }}
+        >
+
+          <div className="underline-wrapper">
+            <h2>About Me</h2>
+            <motion.hr
+              className="underline-title"
+              initial={{ scaleX: 0 }}
+              animate={{ scaleX: 1 }}
+              transition={{ duration: 1, ease: 'easeInOut', delay: 1.5 }}
+            />
+          </div>
           <p>
-            Hi! I'm Louis-Philippe, a passionate developer who loves creating interactive experiences. 
+            Hi! I'm Louis-Philippe, a passionate developer who loves creating interactive experiences.
             I love creating simple and modern app, as well as making feature packed websites.
             When I'm not coding, you can find me exploring new technologies and working on creative projects.
           </p>
-        </div>
+        </motion.div>
 
         {/* Animated Profile Picture */}
         <motion.div
-          className="profile-container"
-          initial={{ y: 0 }}
-          animate={{ y: [0, -10, 0] }}
-          transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+          initial={{ opacity: 0, y: -40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+          viewport={{ once: true, amount: 0.2 }}
         >
-          <img src={profilePic} alt="Profile" className="profile-pic" />
+          <motion.div
+            className="profile-container"
+            initial={{ y: 0 }}
+            animate={{ y: [0, -10, 0] }}
+            transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+          >
+            <img src={profilePic} alt="Profile" className="profile-pic" />
+          </motion.div>
         </motion.div>
       </div>
     </section>
